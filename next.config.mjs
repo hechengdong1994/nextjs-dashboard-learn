c17271c402d0e95a5cbf126ba9708abb3a80683e
ChengDong He <569186076@qq.com>
2024-08-07 09:57:38 +0000

    add demo of part10 Partial Prerendering

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  // 通过next.config.mjs文件添加ppr选项为您的 Next.js 应用程序启用 PPR
+  // 'incremental'值允许您对特定路由采用 PPR。
+  experimental: {
+    ppr: 'incremental'
+  }
+};
 
 export default nextConfig;
